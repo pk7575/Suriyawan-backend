b03740eb1c4e7127dedc89227f837ae20286dbe0
pk7575 <pradeepseth646@gmail.com>
2025-06-12 23:10:10 +0530

    Update ownerRoutes.js

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { ping } = require('../controllers/ownerController');
-
-router.get('/ping', ping);
-
-module.exports = router;
-// routes/ownerRoutes.js
-const express = require('express');
-const router = express.Router();
 const { loginOwner } = require('../controllers/ownerController');
 
-// Ping route
-router.get('/ping', (req, res) => {
-  res.json({ message: '✅ Owner route is live' });
-});
-
-// Login route
 router.post('/login', loginOwner);
 
 module.exports = router;
