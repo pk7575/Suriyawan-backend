18e8e7dd5f6455341f4958172fc277748ce273fb
pk7575 <pradeepseth646@gmail.com>
2025-06-12 18:57:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,3 +19,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Suriyawan Saffari backend is working ✅");
+});
