08b2b21171f0b581aef20885e262e4fc18fea054
pk7575 <pradeepseth646@gmail.com>
2025-06-12 21:35:16 +0530

    Update ownerController.js

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -1,16 +1,5 @@
 const Owner = require('../models/Owner');
 
-const loginOwner = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const owner = await Owner.findOne({ email });
-    if (!owner || owner.password !== password) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-    res.json({ message: 'Login successful', owner });
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
-  }
+exports.ping = (req, res) => {
+  res.status(200).json({ message: '✅ Owner route is live' });
 };
-
-module.exports = { loginOwner };
