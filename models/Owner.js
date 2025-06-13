9bf219863ec2decce4cb671e363495b32ac110fd
pk7575 <pradeepseth646@gmail.com>
2025-06-13 08:09:10 +0530

    Update Owner.js

--- models/Owner.js
+++ models/Owner.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const ownerSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  pin: { type: String, default: '' }
+  email: String,
+  password: String
 });
 
-module.exports = mongoose.model('Owner', ownerSchema);
+export default mongoose.model("Owner", ownerSchema);
