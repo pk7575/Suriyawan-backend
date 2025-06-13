ba2fef55aa25be2cac0b7e0720248db7e5da33c3
pk7575 <pradeepseth646@gmail.com>
2025-06-13 12:12:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 // Server start
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
