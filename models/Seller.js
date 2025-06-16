3c751a757c507628621cb81d8a73ad354427107e
pk7575 <pradeepseth646@gmail.com>
2025-06-16 12:34:27 +0530

    Update Seller.js

--- models/Seller.js
+++ models/Seller.js
@@ -6,23 +6,46 @@ const sellerSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      trim: true
+      trim: true,
+      lowercase: true
     },
+
     password: {
       type: String,
       required: true
     },
+
     category: {
       type: String,
       default: ""
     },
+
     pincode: {
       type: String,
       default: ""
     },
+
     phase: {
       type: String,
       default: "Not Set"
+    },
+
+    imageUrl: {
+      type: String,
+      default: "" // profile image for UI
+    },
+
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      default: ""
+    },
+
+    mobile: {
+      type: String,
+      trim: true,
+      default: ""
     }
   },
   { timestamps: true }
